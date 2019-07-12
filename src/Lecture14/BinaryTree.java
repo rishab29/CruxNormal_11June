d5a23dacb34dae61442c0a76468da7462fb996a5
rishab29 <rishab.kapoor.29@gmail.com>
2019-07-12 12:32:51 +0530

    Code For Lecture4

---
+++ src/Lecture14/BinaryTree.java
@@ -0,0 +1,237 @@
+package Lecture14;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class BinaryTree {
+	private class Node {
+		int data;
+		Node left;
+		Node right;
+
+		Node(int data, Node left, Node right) {
+			this.data = data;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	private Node root;
+	private int size;
+
+	public BinaryTree() {
+		Scanner s = new Scanner(System.in);
+		this.root = this.takeInput(s, null, false);
+	}
+
+	private Node takeInput(Scanner s, Node parent, boolean isLeftOrRight) {
+		if (parent == null) {
+			System.out.println("Enter the data for the root node");
+		} else {
+			if (isLeftOrRight) {
+				System.out.println("Enter the data for the left child of " + parent.data);
+			} else {
+				System.out.println("Enter the data for the right child of " + parent.data);
+			}
+		}
+
+		int data = s.nextInt();
+		Node node = new Node(data, null, null);
+		this.size++;
+
+		System.out.println("Do you have left child of " + node.data);
+		boolean choice = s.nextBoolean();
+		if (choice) {
+			node.left = this.takeInput(s, node, true);
+		}
+
+		System.out.println("Do you have right child of " + node.data);
+		choice = s.nextBoolean();
+		if (choice) {
+			node.right = this.takeInput(s, node, false);
+		}
+
+		return node;
+
+	}
+
+	public void display() {
+		this.display(this.root);
+	}
+
+	private void display(Node node) {
+		if (node.left != null) {
+			System.out.print(node.left.data + "=>");
+		} else {
+			System.out.print("END=>");
+
+		}
+		System.out.print(node.data);
+		if (node.right != null) {
+			System.out.print("<=" + node.right.data);
+		} else {
+			System.out.print("<=END");
+
+		}
+		System.out.println();
+		if (node.left != null) {
+			this.display(node.left);
+		}
+
+		if (node.right != null) {
+			this.display(node.right);
+		}
+
+	}
+
+	public int size2() {
+		return this.size2(this.root);
+	}
+
+	private int size2(Node node) {
+		if (node == null) {
+			return 0;
+		}
+		int lsize = this.size2(node.left);
+		int rsize = this.size2(node.right);
+		int mysize = lsize + rsize + 1;
+		return mysize;
+	}
+
+	public int max() {
+		return this.max(this.root);
+	}
+
+	private int max(Node node) {
+		if (node == null) {
+			return Integer.MIN_VALUE;
+		}
+
+		int lmax = this.max(node.left);
+		int rmax = this.max(node.right);
+		int mymax = Math.max(node.data, Math.max(lmax, rmax));
+		return mymax;
+	}
+
+	public int min() {
+		return this.min(this.root);
+	}
+
+	private int min(Node node) {
+		if (node == null) {
+			return Integer.MAX_VALUE;
+		}
+
+		int lmin = this.min(node.left);
+		int rmin = this.min(node.right);
+		int mymin = Math.min(node.data, Math.min(lmin, rmin));
+		return mymin;
+	}
+
+	public int height() {
+		return this.height(this.root);
+	}
+
+	private int height(Node node) {
+		if (node == null) {
+			return -1;
+		}
+
+		int lheight = this.height(node.left);
+		int rheight = this.height(node.right);
+		int myheight = Math.max(lheight, rheight) + 1;
+		return myheight;
+	}
+
+	public boolean find(int data) {
+		return this.find(this.root, data);
+	}
+
+	private boolean find(Node node, int data) {
+		if (node == null) {
+			return false;
+		}
+		if (node.data == data) {
+			return true;
+		} else if (this.find(node.left, data)) {
+			return true;
+		} else if (this.find(node.right, data)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void mirror() {
+		this.mirror(this.root);
+	}
+
+	private void mirror(Node node) {
+		if (node == null) {
+			return;
+		}
+		Node temp = node.left;
+		node.left = node.right;
+		node.right = temp;
+		this.mirror(node.left);
+		this.mirror(node.right);
+	}
+
+	public void preOrder() {
+		this.preOrder(this.root);
+	}
+
+	private void preOrder(Node node) {
+		if (node == null) {
+			return;
+		}
+		System.out.print(node.data + " ");
+		preOrder(node.left);
+		preOrder(node.right);
+	}
+
+	public void inOrder() {
+		this.inOrder(this.root);
+	}
+
+	private void inOrder(Node node) {
+		if (node == null) {
+			return;
+		}
+
+		inOrder(node.left);
+		System.out.print(node.data + " ");
+		inOrder(node.right);
+	}
+
+	public void postOrder() {
+		this.postOrder(this.root);
+	}
+
+	private void postOrder(Node node) {
+		if (node == null) {
+			return;
+		}
+
+		postOrder(node.left);
+		postOrder(node.right);
+		System.out.print(node.data + " ");
+	}
+
+	public void levelOrder() {
+
+		LinkedList<Node> queue = new LinkedList<>();
+		queue.add(this.root);
+		while (!queue.isEmpty()) {
+			Node rv = queue.remove();
+			System.out.print(rv.data + " ");
+			if (rv.left != null) {
+				queue.add(rv.left);
+			}
+			if (rv.right != null) {
+				queue.add(rv.right);
+			}
+		}
+	}
+
+}
---
+++ src/Lecture14/client.java
@@ -0,0 +1,22 @@
+package Lecture14;
+
+public class client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 50 true 25 true 12 false false true 81 false false true 75 true 48 false false true 16 false false 
+		BinaryTree tree = new BinaryTree();
+		tree.display();
+		
+		System.out.println(tree.size2());
+		System.out.println(tree.max());
+		System.out.println(tree.min());
+		System.out.println(tree.height());
+		
+		System.out.println(tree.find(100));
+		tree.mirror();
+		tree.display();
+
+	}
+
+}
